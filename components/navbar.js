0612396b6ed90286142156eb9154ff6253386d93
lukisoo <84960461+lukisoo@users.noreply.github.com>
2022-04-20 02:11:57 +1200

    added a background and some modules

---
+++ components/navbar.js
@@ -0,0 +1,15 @@
+import { Heading } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
+
+export default function Navbar() {
+  return (
+    <div className="">
+      <h2>navbar</h2>
+        <div className="navbar-button">123</div>
+        <div className="navbar-button">qwe</div>
+        <div className="navbar-button">asd</div>
+        <div className="navbar-button">zxc</div>
+
+    </div>
+  );
+}
---
+++ theme/index.js
@@ -0,0 +1,27 @@
+// theme.js
+import { extendTheme } from '@chakra-ui/react'
+
+// Version 1: Using objects
+const theme = extendTheme({
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        bg: 'gray.400',
+        color: 'white',
+      },
+      // styles for the `a`
+      a: {
+        color: 'teal.500',
+        _hover: {
+          textDecoration: 'underline',
+        },
+      },
+    },
+  },
+})
+
+export default theme
+
+
+/* https://chakra-ui.com/docs/styled-system/theming/customize-theme */

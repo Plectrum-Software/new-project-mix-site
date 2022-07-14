0ffd09be9bc6d2edd0f07d502a128ec2edca68ef
lukisoo <84960461+lukisoo@users.noreply.github.com>
2022-07-14 18:20:28 +1200

    footer update

--- components/footer.js
+++ components/footer.js
@@ -7,8 +7,12 @@ export default function Footer() {
       <div className="footer-top"></div>
 
       <Grid templateColumns="repeat(5, 1fr)" gap={6}>
-        <GridItem w="100%" h="10" bg="blue.500" />
-        <GridItem w="100%" h="10" bg="blue.500" />
+        <GridItem w="100%" h="10" bg="blue.500">
+          <h1>KEEP IN TOUCH!</h1>
+        </GridItem>
+        <GridItem w="100%" h="10" bg="blue.500">
+          <h1>social media here</h1>
+        </GridItem>
       </Grid>
     </div>
   );
